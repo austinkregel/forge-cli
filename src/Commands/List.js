8d4cf7b478ccba4068525b5c304a2f176f6cce31
Austin Kregel <austin@kbcomp.co>
2017-08-23 00:29:06 -0400

    Add list command

--- src/Commands/List.js
+++ src/Commands/List.js
@@ -1,6 +1,34 @@
-const Command = new require('../Command')
-module.exports = class Help extends Command {
+const Command = new require('../Command');
+var Table = require('cli-table');
+var table = new Table({ head: ["id", "name", "description"] });
+
+module.exports = class List extends Command {
     constructor() {
-        super('help');
+        super('list');
+        this.call = super.call
+        this.stripTags = super.stripTags
+        this.argument = super.argument;
+        this.option = super.option;
+        this.describe = super.describe
+        this.signature = 'list';
+        this.handle = () => {
+            let list = [];
+            for(let name in this.systemArguments) {
+                list.push(this.systemArguments[name])
+            }
+
+            let ids = [],
+                names = [],
+                descriptions= []
+            list.forEach((command, id)=> {
+                table.push([
+                    id+1,
+                    command.signature,
+                    command.description || ''
+                ])
+            });
+            console.log(table.toString())
+        }
+
     }
-}
+};
