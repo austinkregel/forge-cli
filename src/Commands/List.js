cb11eed15ecbf1c18e1b2a7b8c7d18b8393a5662
Austin Kregel <austin@kbcomp.co>
2017-08-23 12:51:20 -0400

    Add description to the list command

--- src/Commands/List.js
+++ src/Commands/List.js
@@ -5,12 +5,15 @@ var table = new Table({ head: ["id", "name", "description"] });
 module.exports = class List extends Command {
     constructor() {
         super('list');
+        // I'm probably implementing the classes wrong, but I had to register
+        // the methods like this to make the work because the
         this.call = super.call
         this.stripTags = super.stripTags
         this.argument = super.argument;
         this.option = super.option;
         this.describe = super.describe
         this.signature = 'list';
+        this.description = 'This will list all commands registered with an application.'
         this.handle = () => {
             let list = [];
             for(let name in this.systemArguments) {
