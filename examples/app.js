97b70de203c1baed44dd42e147ff756eaccef268
Austin Kregel <austin@kbcomp.co>
2017-11-04 18:59:01 -0400

    Update example

--- examples/app.js
+++ examples/app.js
@@ -1,7 +1,7 @@
 let Application = require('forge-cli');
 
-Application.command('http:post {url} {--https}', function() {
-    console.log(this.argument('url'))
+Application.command('basicCommand', function() {
+    console.log('This is a basic command.')
 }).describe('this is a command');
 
 Application.register(__dirname, [
