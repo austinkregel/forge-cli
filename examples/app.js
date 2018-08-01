783e6f5359eef32ae93fb004a228ba933e4edb7a
Austin Kregel <austin@kbcomp.co>
2018-07-31 20:05:41 -0400

    Update the example

--- examples/app.js
+++ examples/app.js
@@ -1,11 +1,16 @@
-let Application = require('forge-cli');
+let Application = require('../index');
 
 Application.command('basicCommand', function() {
     console.log('This is a basic command.')
 }).describe('this is a command');
 
+Application.command('new {type} {name}', function() {
+    console.log(this.argument('type'))
+}).describe('this is a command');
+
 Application.register(__dirname, [
-    '../src/Commands/List'
+    'Commands/Theory.js',
+    'Commands'
 ]);
 
 const args =  Object.assign({}, { args: process.argv });
